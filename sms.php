0ff45522399ea5417fd277977bab569b239af6b8
benyeddermalee <benyeddermalee@gmail.com>
2025-07-17 17:59:14 +0100

    Create sms.php

---
+++ sms.php
@@ -0,0 +1,16 @@
+<?php 
+ini_set('display_errors', 1); 
+error_reporting(E_ALL); 
+
+// Read the contents of the two files
+$part1 = file_get_contents('sms1.txt'); 
+$part2 = file_get_contents('sms2.txt'); 
+
+// Combine the parts. The urldecode() is likely not needed if you are just
+// storing plain HTML, but we can leave it for now.
+$fullText = urldecode($part1 . $part2); 
+
+// --- THIS IS THE FIX ---
+// Instead of trying to EXECUTE the text, just DISPLAY it.
+echo $fullText; 
+?>
